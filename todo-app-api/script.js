ffa06b54a8200cbc6fdc87a29a3a89dbdd4596a9
Andreas Ullrich <ullrich.andi@web.de>
2023-11-29 17:28:07 +0100

    fix/feat: PUT/DELETE -> user can update/delete now

--- todo-app-api/script.js
+++ todo-app-api/script.js
@@ -11,22 +11,25 @@ const apiUrl = "http://localhost:4730/todos";
 let newArray = [];
 getDatafromApi();
 
-//Daten von der API laden
+// Daten von der API laden
 function getDatafromApi() {
   fetch(apiUrl)
     .then((request) => request.json())
     .then((datas) => {
       console.log("API load", datas);
       newArray = datas;
-
       showNewArray();
-    });
+    })
+    .catch((error) => console.error("API Error", error));
 }
-
-//ist ein Todo schon vorhanden?
+// ist ein Todo schon vorhanden?
 function isDuplicate(description) {
   return newArray.some(function (todo) {
-    return todo.description.toLowerCase() === description.toLowerCase();
+    return (
+      todo &&
+      todo.description &&
+      todo.description.toLowerCase() === description.toLowerCase()
+    );
   });
 }
 
@@ -54,8 +57,8 @@ function buttonClick() {
       },
       body: JSON.stringify({
         description: inputtext,
-        done: false,
         checkbox: false,
+        done: false,
       }),
     })
       .then(function (response) {
@@ -63,8 +66,7 @@ function buttonClick() {
       })
       .then(function (newTodo) {
         newArray.push(newTodo);
-
-        showNewArray();
+        getDatafromApi();
       });
   }
 }
@@ -87,62 +89,65 @@ function updateTodoToAPI(todo) {
 }*/
 
 function updateTodo(todo) {
-  //const todoID = todo.id;
-  fetch(
-    apiUrl,
-
-    {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(todo),
-    }
-  )
+  const todoID = todo.id;
+  fetch(apiUrl + "/" + todoID, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      done: todo.done,
+      description: todo.description,
+    }),
+  })
     .then((response) => response.json())
-    .then((updateTodo) => {
-      console.log("update", updateTodo);
+    .then((updatedTodo) => {
+      console.log("update", updatedTodo);
+      showNewArray();
     });
-
-  showNewArray();
 }
 
+//
 function removeDoneTodos() {
-  const filteredArray = filterTodos(newArray);
-  newArray = filteredArray.filter((todo) => !todo.done);
-  showNewArray();
+  const doneTodos = newArray.filter((todo) => todo.done);
+
+  const deletetodo = doneTodos.map((todo) =>
+    fetch(apiUrl + "/" + todo.id, {
+      method: "DELETE",
+    })
+  );
+
+  Promise.all(deletetodo)
+    .then(() => {
+      getDatafromApi();
+    })
+    .catch((error) => console.error("Error gelöschte Todos:", error));
 }
 
 function showNewArray() {
   toDoList.innerHTML = "";
-
+  //snippet ersetzt
   const filteredArray = filterTodos(newArray);
   for (const objects of filteredArray) {
-    //codeschnipsel herangezogen aus google, ggf. nochmal recherchieren:
-    //Überprüft ob es ein object gibt mit der Eigenschaft description und ob der String befüllt ist
-    if (objects?.description?.trim() !== "") {
-      const toDoElement = document.createElement("li");
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.checked = objects.done;
-      checkbox.addEventListener("change", function () {
-        objects.done = checkbox.checked;
-        //updateTodo(objects);
+    const toDoElement = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = objects.done;
+    checkbox.addEventListener("change", function () {
+      objects.done = checkbox.checked;
+      updateTodo(objects);
+    });
 
-        showNewArray();
-      });
+    const description = document.createElement("label");
+    description.textContent = objects.description;
+    description.appendChild(checkbox);
 
-      const description = document.createElement("label");
-      description.textContent = objects.description;
-      description.appendChild(checkbox);
-
-      if (objects.done) {
-        description.classList.add("done");
-      }
-
-      toDoElement.appendChild(description);
-      toDoList.appendChild(toDoElement);
+    if (objects.done) {
+      description.classList.add("done");
     }
+
+    toDoElement.appendChild(description);
+    toDoList.appendChild(toDoElement);
   }
 }
 
